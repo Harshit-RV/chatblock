1c9bbe2109a24d12fb0164b59b80fdf859ea203f
Aakash Jain <aakashjain245@gmail.com>
2024-03-29 23:55:08 +0530

    profile updated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Login from './Login'
 import {useNavigate } from 'react-router-dom'
 import axios from 'axios';
+import { useState,useEffect } from 'react';
 
 
 
 function Profile() {
-
   const navigate = useNavigate();
 
   const logOut = async () => {
