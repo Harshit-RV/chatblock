a8abf805bf234e41a9445a6aed71f3a79a256ba6
iamaayushijain <iamaayushirocks@gmail.com>
2024-03-31 06:25:02 +0530

    delete option added in contacts

--- src/components/Transanctions.jsx
+++ src/components/Transanctions.jsx
@@ -36,11 +36,11 @@ function Transactions() {
   }
   
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center px-4">
-  <div className="max-w-full md:max-w-800 lg:max-w-4xl xl:max-w-4xl bg-gray-300 rounded-lg shadow-lg p-8 mb-8 overflow-y-auto mt-5">
+    <div className="w-screen h-screen flex flex-col items-center justify-center ">
+  <div className="max-w-4xl w-full bg-gray-300 rounded-lg shadow-lg p-8 mb-8 overflow-y-auto mt-5">
     <h1 className="text-2xl font-bold mb-4 uppercase text-center">Past Transactions</h1>
     {transactions.map((transaction, index) => (
-      <div key={index} className="bg-white p-4 rounded-lg shadow-md border border-gray-300 mb-4">
+      <div key={index} className="bg-white p-4 rounded-lg shadow-md border border-gray-300 flex flex-col gap-2">
         <button onClick={toggleButton} className="bg-blue-500 hover:bg-blue-700 text-white text-xs md:text-sm lg:text-base font-bold py-2 px-4 rounded mb-2 md:mb-0 md:mr-4">
           {isToggled ? 'txid' : transaction.txid}
         </button>
