7081f634e5cb55924c2e9703c48464a8eb565fb7
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2024-03-31 05:58:50 +0530

    Contacts improvement

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -21,23 +21,34 @@ const ContactsPage = () => {
     const [ somethingWentWrongMsg, setSomethingWentWrongMsg ] = useState(false);
     const [ accountAdded, setAccountAdded ] = useState(false);
     const [ contactsList, setContactsList ] = useState([]);
+    const [ changeName, setChangeName ] = useState(false)
 
     const addContact = async (name, paymail) => {
         const email = localStorage.getItem('email');
 
-        // try {
+        try {
             const data = await getDocs(collection(db, 'contacts'));
 
             var docRefId;
             let tempList = [];
+            let contactFound = false;
 
             data.docs.map((doc) => {
                 docRefId = doc.id;
                 doc.data().contacts.map((contact) => {
+                    if (contact.name == name){
+                        setChangeName(true)
+                        contactFound = true;
+                        return;
+                    }
                     tempList.push(contact)
                     // console.log(contact)
                 })
             })
+
+            if (contactFound) {
+                return;
+            }
 
             tempList.push({
                 name: name,
@@ -54,9 +65,9 @@ const ContactsPage = () => {
 
 
             // const existingCodes = data.docs.map(doc => [doc.id, doc.data().gameCode]);
-        // } catch (error) {
-        //     console.log('error')
-        // }
+        } catch (error) {
+            console.log('error')
+        }
     }
 
     useEffect(() => {
@@ -97,6 +108,12 @@ const ContactsPage = () => {
           setSomethingWentWrongMsg(false);
         }, 5000);
       }, [somethingWentWrongMsg]);
+      
+      useEffect(() => {
+        setTimeout(() => {
+          setChangeName(false);
+        }, 5000);
+      }, [changeName]);
     
       useEffect(() => {
         setTimeout(() => {
@@ -136,6 +153,7 @@ const ContactsPage = () => {
                     <input onChange={(e) => setPaymail(e.target.value)} placeholder="Paymail" required type="text" id="name" className="mb-5 w-80 my-2 bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"/>  
 
                     {somethingWentWrongMsg === true ? <div className="text-red-500 mt-2">Something went wrong. Please try again</div> : <p></p>}
+                    {changeName === true ? <div className="text-red-500 mt-2">A contact by this name already exists</div> : <p></p>}
                     {accountAdded === true ? <div className="text-green-500 mt-2">Account added successfully.</div> : <p></p>}
                     
                     <button onClick={() => addContact(name, paymail)} className=" flex justify-start mb-28 py-2.5 px-6  text-sm font-medium text-gray-900  bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-red-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
